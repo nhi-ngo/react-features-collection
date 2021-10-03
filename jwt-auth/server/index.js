692a947bcf4096246e13b8e40fb0c29150317379
nhi-ngo <nhingo.y@gmail.com>
2021-10-03 11:51:42 -0400

    refresh token

--- jwt-auth/server/index.js
+++ jwt-auth/server/index.js
@@ -29,6 +29,42 @@ app.get('/', (req, res) => {
 	res.send('Welcome to JWT');
 });
 
+let refreshTokens = [];
+
+/* Refresh token */
+app.post('/api/refresh', (req, res) => {
+	// take the refreshed token from user
+	const refreshToken = req.body.token;
+
+	// send error if there is no token or it's invalid
+	if (!refreshToken) {
+		return res.status(401).json('You are not authenticated!');
+	}
+	if (!refreshTokens.includes(refreshToken)) {
+		return res.status(403).json('Refresh token is not valid');
+	}
+	jwt.verify(refreshToken, 'myRefreshSecretKey', (err, user) => {
+		err & console.log(err);
+		refreshTokens = refreshTokens.filter(token !== refreshToken);
+
+		// if everything is ok, create both new access token and refresh token, and send to user
+		const newAccessToken = generateAccessToken(user);
+		const newRefreshToken = generateRefreshToken(user);
+
+		refreshTokens.push(newRefreshToken);
+
+		res.status(200).json({ accessToken: newAccessToken, refreshToken: newRefreshToken });
+	});
+});
+
+const generateAccessToken = (user) => {
+	return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, 'mySecretKey', { expiresIn: '15m' });
+};
+
+const generateRefreshToken = (user) => {
+	return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, 'myRefreshSecretKey');
+};
+
 app.post('/api/login', (req, res) => {
 	const { username, password } = req.body;
 
@@ -38,11 +74,15 @@ app.post('/api/login', (req, res) => {
 
 	/* Generate access token */
 	if (user) {
-		const accessToken = jwt.sign({ id: user.id, isAdmin: user.isAdmin }, 'mySecretKey');
+		const accessToken = generateAccessToken(user);
+		const refreshToken = generateRefreshToken(user);
+		refreshTokens.push(refreshToken);
+
 		res.json({
 			username: user.username,
 			isAdmin: user.isAdmin,
 			accessToken,
+			refreshToken,
 		});
 	} else {
 		res.status(400).json('Username or password incorrect');
@@ -55,16 +95,16 @@ app.post('/api/login', (req, res) => {
 			const token = authHeader.split(' ')[1];
 			jwt.verify(token, 'mySecretKey', (err, user) => {
 				if (err) {
-          // Having an invalid token
+					// Having an invalid token
 					return res.status(403).json('Token is not valid!');
 				}
 
-        // assign the payload
+				// assign the payload
 				req.user = user;
 				next();
 			});
 		} else {
-      // No token available
+			// No token available
 			res.status(401).json('You are not authenticated!');
 		}
 	};
