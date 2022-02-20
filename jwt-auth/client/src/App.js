2d19c5ef3f17af1f87b5f6af796cc779862d5896
nhi-ngo <nhingo.y@gmail.com>
2022-02-19 22:37:59 -0500

    initial setup

--- jwt-auth/client/src/App.js
+++ jwt-auth/client/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/login', { username, password });
+      console.log(response.data);
+      setUser(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {user ? (
+        <div className="home">
+          <span>
+            Welcome to the <b>{user.isAdmin ? 'admin' : 'user'}</b> dashboard <b>{user.username}</b>
+            .
+          </span>
+          <span>Delete Users:</span>
+          <button className="deleteButton" type="submit">
+            Delete John
+          </button>
+          <button className="deleteButton" type="submit">
+            Delete Jane
+          </button>
+          {error && <span className="error">You are not allowed to delete this user!</span>}
+          {success && <span className="success">User has been deleted successfully...</span>}
+        </div>
+      ) : (
+        <div className="login">
+          <form onSubmit={handleSubmit}>
+            <span className="formTitle">Lama Login</span>
+            <input
+              type="text"
+              placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className="submitButton">
+              Login
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
---
+++ search-filter/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" className="search" placeholder='Search...' />
+      <ul>
+        <li className="listItem">John</li>
+        <li className="listItem">Joe</li>
+        <li className="listItem">Mike</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
